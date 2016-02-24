481fba0f9827f7bf0ed5fb95e06057d1ae92b29c
me <RileyR387@users.noreply.github.com>
2016-02-24 00:21:12 -0600

    cleaned up that old stuff from school

--- src/BigInt.h
+++ src/BigInt.h
@@ -1,5 +1,4 @@
 // Riley Raschke
-// Class::BigInt
 
 #ifndef BigInt_h
 #define BigInt_h
@@ -15,35 +14,35 @@ typedef enum {NEGATIVE, ZERO, POSITIVE} Sign;
 
 class BigInt
 {
-	friend ostream& operator<<( ostream& output, const BigInt& );
-	friend istream& operator>>( istream& input, BigInt& );
+   friend ostream& operator<<( ostream& output, const BigInt& );
+   friend istream& operator>>( istream& input, BigInt& );
 public:
-	BigInt();										// constructor; digits = 0
-	BigInt( int num );								// constructor; digits = num
-	BigInt( const string str );						// constructor; digits = str
-	BigInt( const BigInt& other );					// copy constructor
-	
-	bool    operator==( const BigInt& rhs ) const;	// Equality
-	bool    operator< ( const BigInt& rhs ) const;	// Less Than
-	
-	BigInt  operator+ ( const BigInt& rhs ) const;	// Addition
-	BigInt  operator- ( const BigInt& rhs ) const;	// Subtraction
-	BigInt  operator* ( char digit ) const;			// Multiplication by digit
-													// '0' <= digit <= '9'
-	BigInt  operator* ( const BigInt& rhs ) const;	// Multiplication
-	BigInt  operator/ ( const BigInt& rhs ) const;	// Division
-	BigInt  operator% ( const BigInt& rhs ) const;	// Modulus
-	BigInt  operator^ ( const int power ) const;	// Exponent
+   BigInt();                                        // constructor; digits = 0
+   BigInt( int num );                               // constructor; digits = num
+   BigInt( const string str );                      // constructor; digits = str
+   BigInt( const BigInt& other );                   // copy constructor
 
-	BigInt Factorial() const;		
-	vector<BigInt> FibonacciSequence(const BigInt& n) const; 
-	vector<BigInt> FibonacciSequence(const BigInt& startSeries, const BigInt& endSeries) const;
-	BigInt Square() const;			
-	BigInt Cube() const;			
-	
+   bool    operator==( const BigInt& rhs ) const;   // Equality
+   bool    operator< ( const BigInt& rhs ) const;   // Less Than
+
+   BigInt  operator+ ( const BigInt& rhs ) const;   // Addition
+   BigInt  operator- ( const BigInt& rhs ) const;   // Subtraction
+   BigInt  operator* ( char digit ) const;          // Multiplication by digit
+                                                    // '0' <= digit <= '9'
+   BigInt  operator* ( const BigInt& rhs ) const;   // Multiplication
+   BigInt  operator/ ( const BigInt& rhs ) const;   // Division
+   BigInt  operator% ( const BigInt& rhs ) const;   // Modulus
+   BigInt  operator^ ( const int power ) const;     // Exponent
+
+   BigInt Factorial() const;
+   vector<BigInt> FibonacciSequence(const BigInt& n) const;
+   vector<BigInt> FibonacciSequence(const BigInt& startSeries, const BigInt& endSeries) const;
+   BigInt Square() const;
+   BigInt Cube() const;
+
 private:
-	Sign sign;                                     // Sign of #
-	deque<char> digits;                            // Deque of digits of #
+   Sign sign;
+   deque<char> digits;
 };
 
 #endif
